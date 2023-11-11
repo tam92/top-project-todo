a086f90f1beb600e130139c2ce6341e1facdcb23
Tamara <29234416+tam92@users.noreply.github.com>
2023-11-11 21:53:49 +0100

    Added CSS loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,17 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+        {   // dependencias para cargar CSS
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        },
+    ],
   },
 };
